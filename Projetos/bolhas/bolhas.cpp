5dfc1e7756095234b1d31d70ad6713fc2f2981b5
Felipe-Omar <felipeomar.c@gmail.com>
2017-09-09 22:33:14 -0300

    teste

--- Projetos/bolhas/bolhas.cpp
+++ Projetos/bolhas/bolhas.cpp
@@ -4,6 +4,8 @@
 using namespace cv;
 using namespace std;
 
+//Testando 123
+
 int main(int argc, char** argv){
   Mat image;																					//Imagem a ser modificada
 	int width, height;																	//Largura e altura da imagem
