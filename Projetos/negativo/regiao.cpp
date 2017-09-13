952fc3efca5f5fbd660fe59c53ad76707fdd3dce
Felipe-Omar <felipeomar.c@gmail.com>
2017-09-13 12:39:32 -0300

    update

--- Projetos/negativo/regiao.cpp
+++ Projetos/negativo/regiao.cpp
@@ -5,14 +5,14 @@ using namespace cv;
 using namespace std;
 
 int main(int, char**){
-  Mat image;
-  Vec3b val;
+	Mat image;
+	Vec3b val;
 
 //carregando a imagem
 
-  image= imread("pp.png",CV_LOAD_IMAGE_GRAYSCALE); 
-  if(!image.data)
-    cout << "nao abriu pp.png" << endl;
+	image= imread("pp.png",CV_LOAD_IMAGE_GRAYSCALE); 
+	if(!image.data)
+		cout << "nao abriu pp.png" << endl;
 
 //declarando variáveis para inserção dos pontos pelo usuário
 
@@ -27,46 +27,46 @@ int main(int, char**){
 		cin >> x1;
 		if(x1 >= image.rows || x1 < 0)
 			cout << "Número Inválido :). Insira Novamente." << endl;
-		}
-		while(x1 >=image.rows);
+	}
+	while(x1 >=image.rows);
 
 	do{ 
 		cout << "digite o primeiro ponto (y):" << endl;
 		cin >> y1;
 		if(y1 >= image.cols || y1 < 0)
 			cout << "Número Inválido :). Insira Novamente." << endl;
-		}
-		while(y1 >=image.cols);
+	}
+	while(y1 >=image.cols);
 
 	do{ 
 		cout << "digite o segundo ponto (x):" << endl;
 		cin >> x2;
 		if(x2 >= image.rows || x2 < 0)
 			cout << "Número Inválido :). Insira Novamente." << endl;
-		}
-		while(x2 >=image.rows);
+	}
+	while(x2 >=image.rows);
 
 	do{ 
 		cout << "digite o segundo ponto (y):" << endl;
 		cin >> y2;
 		if(y2 >= image.cols || y2 < 0)
 			cout << "Número Inválido :). Insira Novamente." << endl;
-		}
-		while(y2 >=image.cols);
-	
-	//aqui utilizou-se um "for" para fazer a iteração e,
-	//do ponto inicial informado pelo usuário, até o ponto
-	//final, ele irá aplicar o negativo.
+	}
+	while(y2 >=image.cols);
 
-  for(int i=x1; i<x2; i++){
-    for(int j=y1; j<y2; j++){
-      image.at<uchar>(i,j)=255-image.at<uchar>(i,j); //nesta linha ocorre a aplicação do negativo. 
-	  												 //"255" menos o valor do pixel alocado.
-    }
-  }
-  
-	imwrite("points.png",image);
-  imshow("saida", image);  
-  waitKey();
-  return 0;
+//aqui utilizou-se um "for" para fazer a iteração e,
+//do ponto inicial informado pelo usuário, até o ponto
+//final, ele irá aplicar o negativo.
+
+	for(int i=x1; i<x2; i++){
+		for(int j=y1; j<y2; j++){
+image.at<uchar>(i,j)=255-image.at<uchar>(i,j); //nesta linha ocorre a aplicação do negativo. 
+//"255" menos o valor do pixel alocado.
+}
+}
+
+imwrite("points.png",image);
+imshow("saida", image);  
+waitKey();
+return 0;
 }
